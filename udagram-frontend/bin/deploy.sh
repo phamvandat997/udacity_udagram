0039d4fd5c4db51208e616f13c98a32a16263c7e
PhamVanDat <datpham@PhamVanDats-Laptop.local>
2024-11-09 01:54:31 +0700

    ab

--- udagram-frontend/bin/deploy.sh
+++ udagram-frontend/bin/deploy.sh
@@ -1,11 +1,1 @@
-
-#!/bin/bash
-
-# Check if the build directory exists
-if [ -d "./dist/udagram-fronend" ]; then
-  # Sync the build files with the S3 bucket
-  aws s3 sync ./dist/udagram-fronend arn:aws:s3:::udagramweb --delete
-else
-  echo "Build directory does not exist. Please check the path."
-  exit 1
-fi
+aws s3 sync ./udagram-fronend/www arn:aws:s3:::udagramweb --delete
